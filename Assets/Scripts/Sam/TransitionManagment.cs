ba1967a64fa4244deb35109060c3375f182f335b
Spacestarz <samromell96@gmail.com>
2025-09-18 12:38:41 +0200

    added scenemanagment. WIP pull spak to mech soon done

--- Assets/Scripts/Sam/TransitionManagment.cs
+++ Assets/Scripts/Sam/TransitionManagment.cs
@@ -1,48 +1,82 @@
 using NaughtyAttributes;
+using Oculus.Interaction;
 using UnityEngine;
+using UnityEngine.Events;
+using static Oculus.Interaction.TransformerUtils;
+using NaughtyAttributes;
 
 public class TransitionManagment : MonoBehaviour
 {
     //This script will handle the transition between in train and the mech
-    public Camera CenterEyeCamera;
-    public Camera MechCamera;
+    public static TransitionManagment Instance;
 
-   public GameObject FpsChar;
+    public GameObject choChoSpak;
+    public Transform chochoTransform;
+    public UnityEvent onPress;
+    public UnityEvent onRelease;
+    GameObject presser;
+    AudioSource sound;
+    bool isPressed;
 
-
-    void Start()
+    private double maxconstrain = 0.1;
+    private double stopHere;
+    private void Start()
     {
-        CenterEyeCamera.gameObject.SetActive(true);
+        isPressed = false;
 
-        foreach (Transform child in FpsChar.transform)
+        if (Instance == null)
         {
-            child.gameObject.SetActive(false);
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
-        //just makes the camera on (im dying of the error if i have it on on the start )
-        MechCamera.gameObject.GetComponent<Camera>().enabled = true;
-        
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        stopHere = chochoTransform.transform.position.y - 0.1;
+
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isPressed)
+        {
+            if (chochoTransform.transform.position.y <= stopHere)
+            {
+                // isPressed = other.gameObject;
+                //onPress.Invoke();
+                //sound.Play();
+                //isPressed = true;
+                Debug.Log("hello");
+            }
+          
+        }
+    }
+
+    private void Update()
+    {
+        if (chochoTransform.transform.position.y <= stopHere)
+        {
+            // isPressed = other.gameObject;
+            //onPress.Invoke();
+            //sound.Play();
+            //isPressed = true;
+            Debug.Log("Mech time pew");
+        }
+    }
+
 
     [Button]
-    private void MechActive()
+    private void thing()
     {
-        CenterEyeCamera.gameObject.SetActive(false);
+        //world pos + position constrains
+        Debug.Log($"possition of chocho is: {chochoTransform.transform.position}");
 
-        foreach (Transform child in FpsChar.transform)
-        {
-            child.gameObject.SetActive(true);
-        }
-
-        
+        Debug.Log($"will stop at possition: {chochoTransform.transform.position.y - 0.1}");
     }
 
-    [Button]
-    private void InsideTrain()
-    {
-        foreach (Transform child in FpsChar.transform)
-        {
-            child.gameObject.SetActive(false);
-        }
-        CenterEyeCamera.gameObject.SetActive(true);
-    }
+
+
+
 }
