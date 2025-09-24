e02d33a22f73535061b07195971acb4e98366d98
Emma <ewigur@gmail.com>
2025-09-24 12:45:13 +0200

    Object pool - regular bullets & auto aim

--- Assets/Emmas/E_Scripts/ReusableBullet.cs
+++ Assets/Emmas/E_Scripts/ReusableBullet.cs
@@ -1,27 +1,61 @@
 using UnityEngine;
 
+//NOTE: Combining original BulletScript with ObjectPool - Marked up Teo's stuff
 public class ReusableBullet : MonoBehaviour
 {
+    [Header("Bullet properties")]
     [SerializeField]
-    private float Damage;
+    private float Damage; //From Teo's BulletScript
 
-    [SerializeField] private float maxSpeed = 20f;
-    [SerializeField] private float accelerationTime = 2f;
-    [SerializeField] public Vector3 direction;
+    [SerializeField]
+    private float maxSpeed = 20f; //From Teo's BulletScript
 
-    [SerializeField] private float timeUntilDestruction = 1f;
+    [SerializeField]
+    private float accelerationTime = 2f; //From Teo's BulletScript
 
-    private float currentSpeed = 0f;
-    private float elapsedTime = 0f;
+    [SerializeField]
+    public Vector3 direction; //From Teo's BulletScript
+
+    [SerializeField]
+    private float timeUntilDestruction = 1f;
+
+    [SerializeField]
+    private AudioClip shootingSound; //From Teo's BulletScript
+
+
+    private float currentSpeed = 0f; //From Teo's BulletScript
+    private float elapsedTime = 0f; //From Teo's BulletScript
     private ObjectPool objectPool;
+
+    private AudioSource audioSource; //From Teo's BulletScript
+
+    private float baseAudioStrenght; //From Teo's BulletScript
 
 
     private void Start()
     {
         objectPool = FindFirstObjectByType<ObjectPool>();
 
+        //From Teo's BulletScript{
         direction = transform.forward;
+
+        if (GetComponent<AudioSource>() != null)
+        {
+            audioSource = GetComponent<AudioSource>();
+            baseAudioStrenght = audioSource.volume;
+            PlayShooting();
+        }
+        //}
     }
+
+    //From Teo's BulletScript{
+    private void PlayShooting()
+    {
+        audioSource.pitch = Random.Range(0.95f, 1.05f);
+        audioSource.volume = baseAudioStrenght * Random.Range(0.9f, 1.1f);
+        audioSource.Play();
+    }
+    //}
 
     private void OnEnable()
     {
@@ -32,12 +66,14 @@ public class ReusableBullet : MonoBehaviour
 
     private void Update()
     {
+        //From Teo's BulletScript{
         elapsedTime += Time.deltaTime;
 
         float t = Mathf.Clamp01(elapsedTime / accelerationTime);
         currentSpeed = maxSpeed * (1f - Mathf.Exp(-2f * t));
 
         transform.position += direction.normalized * currentSpeed * Time.deltaTime;
+        //} 
     }
 
     void LateUpdate()
@@ -66,7 +102,13 @@ public class ReusableBullet : MonoBehaviour
             return;
         }
 
+        //From Teo's BulletScript 
+        if (other.GetComponent<ExplodingCacti>() != null)
+        {
+            other.GetComponent<ExplodingCacti>().Explode();
+        }
+
         objectPool.ReturnObject(gameObject);
         Debug.Log("Gatling Bullet hit: " + other.gameObject.name);
-    } 
+    }
 }
