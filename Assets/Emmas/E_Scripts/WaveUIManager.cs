49359f95c74a54795f4a5914304f07937a26e238
Emma <ewigur@gmail.com>
2025-10-07 12:01:05 +0200

    Shovel counter + currentWave

--- Assets/Emmas/E_Scripts/WaveUIManager.cs
+++ Assets/Emmas/E_Scripts/WaveUIManager.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WaveUIManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI waveText;
-    [SerializeField] private TextMeshProUGUI TimeUntilNextWaveText;
+    [SerializeField] private List<TextMeshProUGUI> waveText;
+    [SerializeField] private List<TextMeshProUGUI> TimeUntilNextWaveText;
     [SerializeField] private float waveTextDisplayTime = 3f;
     [SerializeField] private float waveTextFadeDuration = 1f;
     [SerializeField] private float waveCountdown;
@@ -19,8 +20,14 @@ public class WaveUIManager : MonoBehaviour
             return;
         }
 
-        TimeUntilNextWaveText.gameObject.SetActive(false);
-        waveText.gameObject.SetActive(false);
+        foreach (var text in waveText)
+            text.gameObject.SetActive(false);
+
+        foreach (var text in TimeUntilNextWaveText)
+            text.gameObject.SetActive(false);
+
+        // TimeUntilNextWaveText.gameObject.SetActive(false);
+        // waveText.gameObject.SetActive(false);
     }
 
     public void ShowWaveStartText(int currentWave)
@@ -37,48 +44,62 @@ public class WaveUIManager : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
 
+        foreach (var waveText in waveText)
         waveText.gameObject.SetActive(true);
-        waveText.text = $"Wave {currentWave}";
+        
+        foreach (var waveText in waveText)
+            waveText.text = $"Wave {currentWave}";
 
         float elapsedTime = 0f;
-        Color originalColor = waveText.color;
+
+        Color originalColor = waveText[0].color;
 
         while (elapsedTime < waveTextFadeDuration)
         {
+            foreach (var waveText in waveText)
             waveText.color = new Color(originalColor.r, originalColor.g, originalColor.b, Mathf.Lerp(0, 1, elapsedTime / waveTextFadeDuration));
+
             elapsedTime += Time.deltaTime;
             yield return null;
         }
-
-        waveText.color = new Color(originalColor.r, originalColor.g, originalColor.b, 1);
+        
+        foreach (var waveText in waveText)
+            waveText.color = new Color(originalColor.r, originalColor.g, originalColor.b, 1);
 
         yield return new WaitForSeconds(waveTextDisplayTime);
 
         elapsedTime = 0f;
         while (elapsedTime < waveTextFadeDuration)
         {
+            foreach (var waveText in waveText)
             waveText.color = new Color(originalColor.r, originalColor.g, originalColor.b, Mathf.Lerp(1, 0, elapsedTime / waveTextFadeDuration));
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
+        foreach (var waveText in waveText)
         waveText.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0);
-        waveText.gameObject.SetActive(false);
+
+        foreach (var waveText in waveText)
+            waveText.gameObject.SetActive(false);
 
     }
     private IEnumerator UpdateCountdown(float timeUntilNextWave)
     {
+        foreach (var TimeUntilNextWaveText in TimeUntilNextWaveText)
         TimeUntilNextWaveText.gameObject.SetActive(true);
         timeUntilNextWave = EnemyWaveManager.GetTimeBetweenWaves(timeUntilNextWave);
         float countdown = timeUntilNextWave;
 
         while (countdown > 0)
         {
+            foreach (var TimeUntilNextWaveText in TimeUntilNextWaveText)
             TimeUntilNextWaveText.text = $"next wave in:{ Mathf.Ceil(countdown)}";
             yield return new WaitForSeconds(1f);
             countdown -= 1f;
         }
 
+        foreach (var TimeUntilNextWaveText in TimeUntilNextWaveText)
         TimeUntilNextWaveText.gameObject.SetActive(false);
     }
 }
