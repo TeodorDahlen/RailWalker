fb2ef8015e5dc020bd60d0bb69311d800cead9c9
TeodorDahlen <Teodor.dahlen@hotmail.com>
2025-10-09 15:30:37 +0200

    Hpbar

---
+++ Assets/HPbarUpdate.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbarUpdate : MonoBehaviour
+{
+    [SerializeField]
+    private Slider HpBar;
+    private Health health;
+
+    private float maxHealth;
+    private void Start()
+    {
+        health = GetComponent<Health>();
+        health.OnDamaged += UpdateBar;
+        maxHealth = health.GetHealth();
+
+        UpdateBar(0f);
+    }
+
+    private void UpdateBar(float amount)
+    {
+        HpBar.value = health.GetHealth() / maxHealth;
+    }
+}
