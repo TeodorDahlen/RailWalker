13852fcc9e7b37b41fd33a2568a60eb3b31513ca
Emma <ewigur@gmail.com>
2025-10-09 15:54:20 +0200

    Ammo Display Gatling

--- Assets/Emmas/E_Scripts/AmmoManager.cs
+++ Assets/Emmas/E_Scripts/AmmoManager.cs
@@ -1,21 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AmmoManager : MonoBehaviour
 {
-    [SerializeField] private TempatureMeter tempMeter;
+    [SerializeField] private List<TempatureMeter> tempMeter;
     public float amountToDeplete = 0.5f; // heat lost per shot
 
     public bool HasAmmo()
     {
-        return tempMeter != null && tempMeter.Heat > 0;
+        if (tempMeter != null)
+        {
+            foreach (var meter in tempMeter)
+            {
+                if (meter.Heat > 0)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+        // return tempMeter != null && tempMeter.Heat > 0;
     }
 
     public void DepleteAmmo(float amount)
     {
         if (tempMeter != null)
         {
-            tempMeter.RemoveHeat(amount);
-            Debug.Log($"Ammo depleted by {amount}. Current Heat: {tempMeter.Heat}");
+            foreach (var meter in tempMeter)
+            {
+
+                meter.RemoveHeat(amount);
+                // Debug.Log($"Ammo depleted by {amount}. Current Heat: {meter.Heat}");
+
+            }
+            // tempMeter.RemoveHeat(amount);
+            // Debug.Log($"Ammo depleted by {amount}. Current Heat: {tempMeter.Heat}");
         }
     }
 }
